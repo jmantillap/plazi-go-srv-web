a9e7372a71d1e40e1ce74a6c786bdb86f8540cd7
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-07-26 11:57:41 -0500

    Clase de struct para la lista de tareas

--- src/tareas.go
+++ src/tareas.go
@@ -8,12 +8,27 @@ type task struct {
 	completado  bool
 }
 
+func (t task) marcarCompleta() {
+	t.completado = true
+}
+
+func (t task) actualizarDescripcion(des string) {
+	t.descripcion = des
+}
+
+func (t task) actualizarNombre(nombre string) {
+	t.nombre = nombre
+}
+
 func main() {
 	t := task{
 		nombre:      "Completar mi curso de go",
 		descripcion: "Esta semana",
 	}
-
 	fmt.Println(t)
+	fmt.Printf("%+v\n", t)
+	t.marcarCompleta()
+	t.actualizarNombre("Finalizar mi curso de go")
+	t.actualizarDescripcion("Ahora mismo")
 	fmt.Printf("%+v\n", t)
 }
