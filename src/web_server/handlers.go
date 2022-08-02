88ff1abfd46a394694853828df248aeab2cc410d
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-01 20:03:38 -0500

    Se agrega metodo post y decodifica

--- src/web_server/handlers.go
+++ src/web_server/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,28 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello Word desde API HOME!")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decorer := json.NewDecoder(r.Body)
+	var metaData MetaData
+	err := decorer.Decode(&metaData)
+	if err != nil {
+		fmt.Fprintf(w, "error : %v", err)
+		return
+	}
+	//fmt.Println(metaData["name"])
+	fmt.Fprintf(w, "Payload %v\n", metaData)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decorer := json.NewDecoder(r.Body)
+	var user User
+	err := decorer.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error : %v", err)
+		return
+	}
+	fmt.Println(user.Name)
+	fmt.Fprintf(w, "Payload %v\n", user)
 }
